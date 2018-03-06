b45dd83f49f34a48e28cd8d5473bdf0c43b8633d
MECO's GitLab mirrorer <_@_.com>
2018-03-06 11:01:56 +0000

    [AUTO-CI] Synchronize with developers' repository

--- 3rdparty/LCT_MixedHinfsynMIMO_LPV/LPV_example.m
+++ 3rdparty/LCT_MixedHinfsynMIMO_LPV/LPV_example.m
@@ -60,22 +60,3 @@ sys.A = A; sys.B = B; sys.C = C; sys.D = D; sys.Ts = 0;
 options = struct('var_deg',1,'var_knots',0,'controller_dependency','a','spec',inf,'objective','wc','verbose',2);
 % set up and solve the LPV control problem
 [K,sol_info] = LPV_unstructured_OF(sys,ny,nu,param,options)
-
-%%
-k1 = 13;
-im2 = 0.625;
-A = [0 0 1 0; 0 0 0 1; -k1*(1/m1), k1*(1/m1), -(c1+c2)/m1, c2/m1; k1*im2, -(k1+k2)*im2, c2*im2, -c2*im2];
-B = [1 0 0 0; 0 1 0 0; 0 0 1/m1 0; 0 0 0 im2]; 
-C = [1 0 0 0; 0 1 0 0; 0 0 0 0; 0 0 0 0; 1 0 0 0; 0 0 0 1];
-D = [zeros(4,2), [0 0; 0 0; 1 0; 0 1]; zeros(2,2), zeros(2,2)];
-sys.A = A; sys.B = B; sys.C = C; sys.D = D; sys.Ts = 0.0001;
-gen_sys = extract_generalized_plant(sys,nu,ny);
-A  = gen_sys.A;  Bw  = gen_sys.Bw;  Bu  = gen_sys.Bu;
-Cz = gen_sys.Cz; Dzw = gen_sys.Dzw; Dzu = gen_sys.Dzu;
-Cy = gen_sys.Cy; Dyw = gen_sys.Dyw; Dyu = zeros(nu,ny);
-nx = gen_sys.nx; nz = gen_sys.nz; nw = gen_sys.nw;
-opts.Ts = 0.0001;
-opts.Dc = 0;
-Info = compute_FO_Hinf_LTI(A,Bw,Bu,Cz,Cy,Dzw,Dzu,Dyw,Dyu,opts)
-
-
